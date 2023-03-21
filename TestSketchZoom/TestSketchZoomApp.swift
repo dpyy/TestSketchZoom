992147a689cdbf758b03f9f44aad41b27b79910b
Gary <gary.fung@me.com>
2023-03-20 21:55:52 -0400

    Initial Commit

---
+++ TestSketchZoom/TestSketchZoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSketchZoomApp.swift
+//  TestSketchZoom
+//
+//  Created by Gary Fung on 2023-03-20.
+//
+
+import SwiftUI
+
+@main
+struct TestSketchZoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
